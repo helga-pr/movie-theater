c603929a0ce58b23371d1e3ada9eece716c4c796
dmitry_kologrivov <dmitry_kologrivov@epam.com>
2016-05-08 13:14:00 +0300

    Fix path

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 error_reporting(E_ALL);
 
 defined( 'APPLICATION_PATH' ) || define( 'APPLICATION_PATH', realpath( dirname( __file__ ) . '/App' ) ) ;
+define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
 
 require_once 'autoload.php';
 require_once APPLICATION_PATH.'/bootstrap.php';
